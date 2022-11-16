47e4ed18842081ff1b74d19f817f0735fadaf75f
Tekyto <Tekyto2014@gmail.com>
2022-11-16 20:27:18 +0200

    js_18_chess-knight+

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function knight(x1, y1, x2, y2) {
-	// write your code here
+	return Math.abs(x2 - x1) === 2 && Math.abs(y2 - y1) === 1 || Math.abs(x2 - x1) === 1 && Math.abs(y2 - y1) === 2;
 }
 
 module.exports = knight;
